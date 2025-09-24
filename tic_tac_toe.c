ac70ce3ad56051a7777b947116d4a6c4c2619cd0
Dilmith.P.H.S <it25102756@my.sliit.lk>
2025-09-24 10:34:01 +0530

    Update game_no2.c

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,66 +1,55 @@
 #include <stdio.h>
-#include <string.h>
 
 #define MIN_SIZE 3
 #define MAX_SIZE 9
-#define CELL_LEN 4
 
-char board[MAX_SIZE][MAX_SIZE][CELL_LEN];
-int boardSize;
-
-void initBoard() {
+void drawBoard(int size) {
     int count = 1;
-    for (int i = 0; i < boardSize; i++) {
-        for (int j = 0; j < boardSize; j++) {
-            sprintf(board[i][j], "%d", count++);
-        }
-    }
-}
-
-void drawBoard() {
-    int i, j;
-    printf("\n");
-    for (i = 0; i < boardSize; i++) {
-        for (j = 0; j < boardSize; j++) {
-            printf(" %2s ", board[i][j]);
-            if (j < boardSize - 1) printf("|");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf(" %2d ", count++);
+            if (j < size - 1) printf("|");
         }
         printf("\n");
-        if (i < boardSize - 1) {
-            for (j = 0; j < boardSize; j++) {
+        if (i < size - 1) {
+            for (int j = 0; j < size; j++) {
                 printf("----");
-                if (j < boardSize - 1) printf("+");
+                if (j < size - 1) printf("+");
             }
             printf("\n");
         }
     }
-    printf("\n");
 }
 
 void gameUserUser() {
     printf("Starting User vs User mode...\n");
 }
+
 void gameUserComputer() {
     printf("Starting User vs Computer mode...\n");
 }
+
 void game3Users() {
     printf("Starting 3 Users mode...\n");
 }
 
 int main() {
-    int choice;
+    int boardSize, choice;
 
     printf("Enter board size (%d to %d): ", MIN_SIZE, MAX_SIZE);
-    if (scanf("%d", &boardSize) != 1 || boardSize < MIN_SIZE || boardSize > MAX_SIZE) {
-        printf("Invalid size!\n");
+    if (scanf("%d", &boardSize) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+    if (boardSize < MIN_SIZE || boardSize > MAX_SIZE) {
+        printf("Invalid size! Must be between %d and %d.\n", MIN_SIZE, MAX_SIZE);
         return 1;
     }
 
-    initBoard();
-    printf("\nTic Tac Toe Board (%dx%d):\n", boardSize, boardSize);
-    drawBoard();
+    printf("\nTic Tac Toe Board (%dx%d):\n\n", boardSize, boardSize);
+    drawBoard(boardSize);
 
-    printf("Select mode:\n");
+    printf("\nSelect mode:\n");
     printf("1. User vs User\n");
     printf("2. User vs Computer\n");
     printf("3. 3 Users\n");
